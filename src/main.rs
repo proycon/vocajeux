0b0da8cc7f4b149d360e58d37c7e74710b785226
Maarten van Gompel <proycon@anaproy.nl>
2018-12-24 22:50:28 +0100

    implementing quiz mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate clap;
+extern crate rand;
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
@@ -7,9 +8,12 @@ extern crate serde_derive;
 use std::fs;
 use std::error::Error;
 use std::fmt;
+use std::iter::Iterator;
+use std::io::BufRead;
 use std::collections::HashMap;
 use clap::{App, Arg, SubCommand};
 use md5::{compute,Digest};
+use rand::random;
 
 /// Vocabulary Item data structure
 #[derive(Serialize, Deserialize)]
@@ -60,6 +64,102 @@ fn list(data: &VocaList, withtranslation: bool, withtranscription: bool) {
     }
 }
 
+///Select a word
+fn select_item(data: &VocaList) -> &VocaItem {
+    let choice: f64 = rand::random::<f64>() * (data.items.len() as f64);
+    let choice: usize = choice as usize;
+    &data.items[choice]
+}
+
+fn getinputline() -> Option<String> {
+    let stdin = std::io::stdin();
+    let response = stdin.lock().lines().next().unwrap().unwrap(); //read one line only
+    if response != "" {
+        return Some(response);
+    } else {
+        return None;
+    }
+}
+
+///Quiz
+fn quiz(data: &VocaList) {
+    let guesses = 3;
+    loop {
+        //select a random item
+        let vocaitem = select_item(data);
+        println!("Translate: {}", vocaitem);
+        let mut correct = false;
+        for _ in 0..guesses {
+            //get response from user
+            if let Some(response) = getinputline() {
+                correct = response == vocaitem.translation;
+                if correct {
+                    break;
+                }
+            } else {
+                break;
+            }
+            println!("Incorrect! Try again (or ENTER to skip)");
+        }
+        if !correct {
+            println!("The correct translation is: {}", vocaitem.translation);
+        }
+    }
+}
+
+fn getquizoptions<'a>(data: &'a VocaList, correctitem: &'a VocaItem) -> (Vec<&'a VocaItem>, usize) {
+    let optioncount = 6;
+    //reserve an index for the correct option
+    let correctindex: f64 = rand::random::<f64>() * (optioncount as f64);
+    let correctindex: usize = correctindex as usize;
+    let options: Vec<&VocaItem>;
+    for i in 0..optioncount {
+        if i == correctindex {
+            options.push(correctitem);
+        } else {
+            loop {
+                let candidate = select_item(data);
+                if candidate.id() != correctitem.id() {
+                    options.push(candidate);
+                    break;
+                }
+            }
+        }
+    }
+    (options, correctindex)
+}
+
+///Multiple-choice Quiz
+fn multiquiz(data: &VocaList) {
+    loop {
+        //select a random item
+        let vocaitem = select_item(data);
+        println!("Translate: {}", vocaitem);
+        let (options, correctindex) = getquizoptions(&data, &vocaitem);
+        for (i, option) in options.enumerate() {
+            println!("{} - {}", i+1, option.translation);
+        }
+        let mut correct = false;
+        //get response from user
+        if let Some(response) = getinputline() {
+            if let Ok(responseindex) = response.parse::<usize>() {
+                correct = responseindex -1 == correctindex;
+                if correct {
+                    break;
+                }
+            } else {
+                println!("Enter a number!");
+            }
+        } else {
+            break;
+        }
+        println!("Incorrect! Try again (or ENTER to skip)");
+        if !correct {
+            println!("The correct translation is: {}", vocaitem.translation);
+        }
+    }
+}
+
 
 fn main() {
     let argmatches = App::new("Vocajeux")
@@ -82,6 +182,9 @@ fn main() {
                          .long("phon")
                          .short("p")
                     ))
+        .subcommand(SubCommand::with_name("quiz")
+                    .about("Simple quiz")
+                    )
         .get_matches();
 
     if let Some(filename) = argmatches.value_of("file") {
@@ -96,6 +199,9 @@ fn main() {
                         } else {
                             list(&data, false, false);
                         }
+                    },
+                    Some("quiz") => {
+                        quiz(&data);
                     },
                     _ => {
                         eprintln!("Nothing to do!");
